e6ea5360fb34c37b2e314357678686c4da4df134
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-07-11 16:41:08 +0200

    [FEAT] basic files structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+
+@extends("default.default")
+
+@section("page_title")
+Cocktails - Choose Yours
+@endsection
+
+    {{-- <img
+        src="{{ Vite::asset("resources/img/duck.jpg") }}"
+        alt="Paperella Laravel Template"
+    /> --}}
