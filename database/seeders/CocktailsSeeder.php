4d13c051d21fcbea17d23da578c5a2bfc3eec7a1
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-11 17:22:19 +0200

    [UPD] seeder

--- database/seeders/CocktailsSeeder.php
+++ database/seeders/CocktailsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Faker\Generator as Faker;
+use App\Models\Cocktail;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,13 @@ class CocktailsSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $cocktail = new Cocktail();
+            $cocktail->name = $faker->lexify('????');
+
+            $cocktail->save();
+        }
     }
 }
