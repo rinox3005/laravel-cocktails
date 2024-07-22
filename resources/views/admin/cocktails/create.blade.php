23e3e10e4a2667382145fba972d951f25c75e898
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-22 17:39:55 +0200

    [UPD] Create

--- resources/views/admin/cocktails/create.blade.php
+++ resources/views/admin/cocktails/create.blade.php
@@ -42,8 +42,7 @@
                 {{-- description --}}
                 <div class="mt-3 form-group">
                     <label for="description">Descrizione</label>
-                    <textarea name="description" id="description" class="form-control"
-                        required>value="{{ old('description', $cocktail->description ?? '') }}"</textarea>
+                    <textarea name="description" id="description" class="form-control" required>{{ old('description', $cocktail->description ?? '') }}</textarea>
                 </div>
 
                 {{-- ingredients --}}
@@ -54,44 +53,46 @@
 
                 {{-- preparation type --}}
                 <div class="mt-3 form-group">
-                    {{-- <label for="type_of_prep">Tipo di preparazione</label>
-                    <input type="text" name="type_of_prep" id="type_of_prep" class="form-control" required> --}}
-                    <select class="form-select" aria-label="Default select example">
+                    <label>Tipo di preparazione</label>
+                    <select name="type_of_prep" class="form-select" aria-label="Default select example">
                         @foreach ($type_of_prep as $prep)
-<option value="{{ $prep }}">{{ $prep }}</option>
-@endforeach
+                            <option value="{{ $prep }}">{{ $prep }}</option>
+                        @endforeach
                     </select>
                 </div>
 
                 {{-- preparation time --}}
                 <div class="mt-3 form-group">
+
                     <label for="serving_temp">Tempo di preparazione</label>
-                    <input value="{{ old('serving_temp', $cocktail->serving_temp ?? '') }}" type="number" name="serving_temp" id="serving_temp" class="form-control" step="0.01"
-                        required>
+                    <input value="{{ old('serving_temp', $cocktail->serving_temp ?? '') }}" type="number"
+                        name="serving_temp" id="serving_temp" class="form-control" step="0.01" required>
                 </div>
 
                 {{-- glass type --}}
                 <div class="mt-3 form-group">
                     <label for="glass_type">Tipo di bicchiere</label>
-                    <input value="{{ old('glass_type', $cocktail->glass_type ?? '') }}" type="text" name="glass_type" id="glass_type" class="form-control">
+                    <input value="{{ old('glass_type', $cocktail->glass_type ?? '') }}" type="text" name="glass_type"
+                        id="glass_type" class="form-control">
                 </div>
 
                 {{-- preparation --}}
                 <div class="mt-3 form-group">
                     <label for="preparation">Preparazione</label>
-                    <textarea name="preparation" id="preparation" class="form-control" required>value="{{ old('preparation', $cocktail->preparation ?? '') }}"</textarea>
+                    <textarea name="preparation" id="preparation" class="form-control" required>{{ old('preparation', $cocktail->preparation ?? '') }}</textarea>
                 </div>
 
                 {{-- needs ice --}}
                 <div class="mt-3 form-group">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="need_ice" id="no_needs_ice">
+                        <input class="form-check-input" type="radio" name="needs_ice" id="no_needs_ice" value="0">
                         <label class="form-check-label" for="no_needs_ice">
                             Senza Ghiaccio
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="need_ice" id="needs_ice" checked>
+                        <input class="form-check-input" type="radio" name="needs_ice" id="needs_ice" value="1"
+                            checked>
                         <label class="form-check-label" for="needs_ice">
                             Con Ghiaccio
                         </label>
@@ -101,13 +102,15 @@
                 {{-- available --}}
                 <div class="mt-3 form-group">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="available" id="not_available">
+                        <input class="form-check-input" type="radio" name="is_available" id="not_available"
+                            value="0">
                         <label class="form-check-label" for="not_available">
                             Non disponibile
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="available" id="available" checked>
+                        <input class="form-check-input" type="radio" name="is_available" id="available" value="1"
+                            checked>
                         <label class="form-check-label" for="available">
                             Disponibile
                         </label>
