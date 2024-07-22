b9b4af91b296e99b4ee5af56176ce26b4581ca64
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-07-22 16:33:38 +0200

    [FIX] web coktails controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Guest\CocktailController;
-use App\Http\Controllers\Admin\CocktaislController;
+use App\Http\Controllers\Admin\CocktailsController;
 use Illuminate\Support\Facades\Route;
 
 /*
