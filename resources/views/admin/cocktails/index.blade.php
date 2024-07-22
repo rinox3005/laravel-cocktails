6c06ce72a00c9c7e10916189089e20a115c9f298
MykolaDekusar <ndekusar97@gmail.com>
2024-07-22 16:28:42 +0200

    [UPD] Added Index, Show, and Modal for deletion of a cocktail

---
+++ resources/views/admin/cocktails/index.blade.php
@@ -0,0 +1,28 @@
+@extends('default.default')
+
+@section('page_title')
+    Cocktails - Choose Yours
+@endsection
+
+@section('main')
+    <div class="row row-cols-lg-4 row-cols-md-3 g-4 justify-content-center my-5">
+        @foreach ($cocktails as $cocktail)
+            <div class="col">
+                <div class="card" style="width: 18rem">
+                    <img class="card-img-top" src="{{ Vite::asset('resources/img/default.jpg') }}"
+                        alt="Paperella Laravel Template" />
+                    <div class="card-body d-grid">
+                        <h5 class="card-title">{{ $cocktail->name }}</h5>
+                        <p class="card-text">{{ $cocktail->description }}</p>
+                        <p class="card-text">{{ $cocktail->type_of_prep }}</p>
+                        <div class="buttons align-self-end">
+                            <a href="{{ route('admin.cocktails.show') }}" class="btn btn-secondary">More Info</a>
+                            <button class="btn btn-danger">Delete Cocktail</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        @include('partials.modal')
+    </div>
+@endsection
--- resources/views/partials/cocktail.blade.php
+++ resources/views/partials/cocktail.blade.php
@@ -1,13 +1,9 @@
 <div class="card" style="width: 18rem">
-    <img
-        class="card-img-top"
-        src="{{ Vite::asset("resources/img/default.jpg") }}"
-        alt="Paperella Laravel Template"
-    />
+    <img class="card-img-top" src="{{ Vite::asset('resources/img/default.jpg') }}" alt="Paperella Laravel Template" />
     <div class="card-body d-grid">
-        <h5 class="card-title">{{ $cocktail["name"] }}</h5>
-        <p class="card-text">{{ $cocktail["description"] }}</p>
-        <p class="card-text">{{ $cocktail["type_of_prep"] }}</p>
+        <h5 class="card-title">{{ $cocktail['name'] }}</h5>
+        <p class="card-text">{{ $cocktail['description'] }}</p>
+        <p class="card-text">{{ $cocktail['type_of_prep'] }}</p>
         <div class="buttons align-self-end">
             <a href="#" class="btn btn-secondary">More Info</a>
             <a href="#" class="btn btn-primary">Add To Cart</a>
